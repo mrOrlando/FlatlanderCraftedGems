97ec5097ad046759b1f3410270fdc5697c2aa967
mrOrlando <juriy88@mail.com>
2016-09-01 08:47:45 +0300

    Отображаются два элемента. Добавлена кнопка Положить в корзину.

--- app/app.js
+++ app/app.js
@@ -3,15 +3,37 @@
     var app = angular.module('store', []);
 
     app.controller('StoreController', function() {
-        alert("Welcome, User!");
-        this.product = gem;
+        this.products = gems;
     });
 
-    var gem = {
-        name: "Dodecahedron",
-        price: 2.95,
-        description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-        soldOut: false
-    };
+    var gems = [
+        {
+            name: "Dodecahedron",
+            price: 2.95,
+            description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+            canPurchase: true,
+        },
+        {
+            name: "Pentagonal Gem",
+            price: 5.95,
+            description: ". . .",
+            canPurchase: false,
+        },
+        {
+            name: 'Azurite',
+            price: 2.95,
+            canPurchase: false,
+        },
+        {
+            name: 'Bloodstone',
+            price: 5.95,
+            canPurchase: true,
+        },
+        {
+            name: 'Zircon',
+            price: 3.95,
+            canPurchase: true,
+        }
+    ];
 
 })();

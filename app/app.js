b3936a661e3278f79644c599817c3a1e6d8e7241
mrOrlando <juriy88@mail.com>
2016-09-02 08:48:32 +0300

    Добавлены вкладки, описание камней. Список камней выводится по возрастанию цены.

--- app/app.js
+++ app/app.js
@@ -30,6 +30,7 @@
         {
             name: 'Azurite',
             price: 2.95,
+            description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
             canPurchase: false,
             images: [
                 'img/azurite-01.gif',
@@ -40,6 +41,7 @@
         {
             name: 'Bloodstone',
             price: 5.95,
+            description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
             canPurchase: true,
             images: [
                 'img/bloodstone-01.gif',
@@ -50,6 +52,7 @@
         {
             name: 'Zircon',
             price: 3.95,
+            description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
             canPurchase: true,
             images: [
                'img/zircon-01.gif',

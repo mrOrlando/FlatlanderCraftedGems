f9b553a5238c0f4c62cf859ba1e2c5f3a4716551
mrOrlando <juriy88@mail.com>
2016-09-03 20:42:31 +0300

    Из вкладки описание создана пользовательская дериктива.

--- app/app.js
+++ app/app.js
@@ -36,6 +36,13 @@
         };
     });
 
+    app.directive("productDescription", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-description.html"
+        };
+    });
+
     var gems = [
         {
             name: "Dodecahedron",

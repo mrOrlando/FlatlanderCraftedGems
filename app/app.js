2d1c0efec74bd4ebc4943c5bdbaaf89c36f6c085
mrOrlando <juriy88@mail.com>
2016-09-04 18:56:33 +0300

    Вкладки вынесены в отдельный файл с созданием директивы с вложенным контроллером.

--- app/app.js
+++ app/app.js
@@ -6,18 +6,6 @@
         this.products = gems;
     });
 
-    app.controller("PanelContoller", function() {
-        this.tab = 1;
-
-        this.selectTab = function(setTab) {
-            this.tab = setTab;
-        };
-
-        this.isSelected = function(checkTab) {
-            return this.tab === checkTab;
-        }
-    });
-
     app.controller("GalleryController", function() {
         this.current = 0;
 
@@ -55,6 +43,25 @@
         restrict: 'A',
         templateUrl: "product-specs.html"
       };
+    });
+
+    app.directive("productPanels", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-panels.html",
+            controller: function() {
+                this.tab = 1;
+
+                this.selectTab = function(setTab) {
+                    this.tab = setTab;
+                };
+
+                this.isSelected = function(checkTab) {
+                    return this.tab === checkTab;
+                }
+            },
+            controllerAs: "panel"
+        };
     });
 
     var gems = [

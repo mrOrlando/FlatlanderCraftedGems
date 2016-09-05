260900ce57d67824ed5ff215c447b8aff312997d
mrOrlando <juriy88@mail.com>
2016-09-05 22:04:18 +0300

    Галерея изображений вынесена в отдельный файл с созданием директивы с вложенным контроллером.

--- app/app.js
+++ app/app.js
@@ -6,11 +6,17 @@
         this.products = gems;
     });
 
-    app.controller("GalleryController", function() {
-        this.current = 0;
-
-        this.setCurrent = function(value) {
-            this.current = value || 0;
+    app.directive("productGallery", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-gallery.html",
+            controller: function() {
+                this.current = 0;
+                this.setCurrent = function(imageNumber) {
+                    this.current = imageNumber || 0;
+                };
+            },
+            controllerAs: "gallery"
         };
     });
 
